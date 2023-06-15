68c01cd496468925f45280dbad1cfbd12f900648
Muhamad-naufal <muhamadnaufal594@gmail.com>
2023-06-15 18:12:04 +0700

    tambahan 2

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,7 +10,7 @@
             <form action="?" method="get" class="form-inline">
                 <div class="input-group ml-auto">
                     <input type="text" name="search" class="form-control" placeholder="Search"
-                        values="<?php= request()->search ?> ?> ?>">
+                        values="<?php= request()->search ?> ?>">
                     <div class="input-group-append">
                         <button class="btn btn-outline-secondary" type="submit">Cari..</button>
                     </div>
